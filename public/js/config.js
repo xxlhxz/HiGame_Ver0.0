34e06f6501c0d33dd7276701a9d0dc588920a3da
xxlhxz <dukeandrewx@126.com>
2015-02-17 19:24:59 +0800

    update id key

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 /**
  * Created by lzw on 14-8-21.
  */
-var appId='ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333';
-var appKey='ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav';
+var appId="u7mfyjit5fl2deab08abbaraczl8so97p4v1b6pndbredch0";//ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333
+var appKey="sg2krlvgf36cbp3gjvbmnmf2hyvk3skk3kmumodil78vdlld";//ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav
